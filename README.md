45a62a28e6d84409099c8aaf9dcb24966a70ab9e
Eurus-Azir <594355134@qq.com>
2024-06-20 23:29:23 +0800

    更新Readme

--- README.md
+++ README.md
@@ -1,19 +1,49 @@
-# 2024.6.8
-## 第一轮迭代v1.5.2
-### 编程语言 
-JAVA
+### 编程语言
 
-### 开发环境  
-Windows10 IDEA 2024.1 MySQL8
+* Java
 
-### 运行环境 
-jdk1.8 nodejs 18
+### 开发环境
 
-### 主要技术 
-vue2 springboot 2 mybatis，Axios
+* Windows10
+  * IDEA 2024.1
+  * MySQL8
+
+### 运行环境
+
+* JDK 1.8 
+* Node.js 18
+
+### 主要技术
+
+* 前端
+  * Vue2 
+  * Axios
+  * element-ui
+  * animate.css
+* 后端
+  * SpringBoot 2 
+  * Mybatis Puls
+
+### 如何运行
+
+* 前端
+  * 进入`myvue/`
+  * `npm i`
+  * `npm run dev`
+* 后端
+  * 使用 IDEA打开 `tutor`
+    * Maven加载依赖
+  * 数据库
+    * 新建数据库`ssm`
+    * 运行`tutor/src/sql/ssm.sql`
+  * 配置
+    * 修改`tutor/src/main/resources/application.yml`中的`username`和`password`
+  * 启动后端
 
 ### 工作进展
+
 第一轮迭代基本实现了Project的各项要求，包括：
+
 1. 教师学生信息注册
 2. 人员分类管理
 3. 教师家教信息与学生家教需求的发布
@@ -22,33 +52,35 @@ vue2 springboot 2 mybatis，Axios
 6. 前后端数据库一致性管理
 
 ### 第二轮迭代预期工作
+
 1. 第二轮迭代我们会尝试设计推荐匹配算法，为用户自动推荐需要的内容
 2. 增加权限管理，对不同权限用户提供不同的操作环境
 3. 美化UI，让系统更加美观便利
 
 ### 第一轮迭代项目界面展示
+
 登录界面：
 
-![登录界面](/myvue/src/assets/images/登录.png)
+![登录界面](images/登录.png)
 
 注册界面：
 
-![注册界面](/myvue/src/assets/images/注册.png)
+![注册界面](images/注册.png)
 
 主页界面：
 
-![主页界面](/myvue/src/assets/images/主页.png)
+![主页界面](images/主页.png)
 
 学生管理界面：
 
-![学生管理界面](/myvue/src/assets/images/学生管理.png)
+![学生管理界面](images/学生管理.png)
 
 教师管理界面：
 
-![教师管理界面](/myvue/src/assets/images/教师管理.png)
+![教师管理界面](images/教师管理.png)
 
 评价反馈界面：
 
-![评价反馈界面](/myvue/src/assets/images/评价反馈.png)
+![评价反馈界面](images/评价反馈.png)
 
 ## 第二轮迭代v2.6.1
--- tutor/src/sql/ssm.sql
+++ tutor/src/sql/ssm.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80037 (8.0.37)
  File Encoding         : 65001
 
- Date: 08/06/2024 20:50:56
+ Date: 20/06/2024 23:28:35
 */
 
 SET NAMES utf8mb4;
@@ -45,28 +45,28 @@ INSERT INTO `demand_publish` VALUES (3, 7, '英语', '高三', '周二', '要求
 -- ----------------------------
 DROP TABLE IF EXISTS `info_publish`;
 CREATE TABLE `info_publish`  (
-    `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `user_id` int NOT NULL COMMENT '用户ID，关联用户ID',
-    `subjects` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '科目',
-    `grades` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '年级',
-    `title` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '标题',
-    `days` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '星期几',
-    `periods` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '时间段',
-    `introduction` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL COMMENT '个人简介或其他信息',
-    `image_url` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '图片url',
-    PRIMARY KEY (`id`) USING BTREE,
-    INDEX `user_id`(`user_id` ASC) USING BTREE,
-    CONSTRAINT `info_publish_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` int NOT NULL COMMENT '用户ID，关联用户ID',
+  `subjects` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '科目',
+  `grades` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '年级',
+  `title` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `days` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '星期几',
+  `periods` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL DEFAULT NULL COMMENT '时间段',
+  `introduction` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL COMMENT '个人简介或其他信息',
+  `image_url` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '图片url',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user_id`(`user_id` ASC) USING BTREE,
+  CONSTRAINT `info_publish_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
+
 -- ----------------------------
 -- Records of info_publish
 -- ----------------------------
-INSERT INTO `info_publish` (`id`, `user_id`, `subjects`, `grades`, `title`, `introduction`, `image_url`, `days`, `periods`) VALUES
-(1, 2, '数学', '高一', '高一数学', '有责任心，从教5年。', '/uploads/1717848365924_OIP.jpg', '星期一,星期三,星期五', '上午'),
-(2, 2, '数学', '高二', '高二数学', '有责任心，从教5年。', '/uploads/1717848365924_OIP.jpg', '星期二,星期四,星期六', '上午'),
-(3, 3, '语文', '高三', '高三语文', '语文教学能手', '/uploads/1717848365924_OIP.jpg', '星期一,星期三', '下午'),
-(4, 5, '英语', '高三', '高三英语', '英语教学能手', '/uploads/1717848365924_OIP.jpg', '星期二,星期四', '下午'),
-(5, 6, '生物', '高三', '高三生物', '高中生物速成', '/uploads/1717848365924_OIP.jpg', '星期三,星期五', '晚上');
+INSERT INTO `info_publish` VALUES (1, 2, '数学', '高一', '高一数学', '星期一,星期三,星期五', '上午', '有责任心，从教5年。', '/uploads/1717848365924_OIP.jpg');
+INSERT INTO `info_publish` VALUES (2, 2, '数学', '高二', '高二数学', '星期二,星期四,星期六', '上午', '有责任心，从教5年。', '/uploads/1717848365924_OIP.jpg');
+INSERT INTO `info_publish` VALUES (3, 3, '语文', '高三', '高三语文', '星期一,星期三', '下午', '语文教学能手', '/uploads/1717848365924_OIP.jpg');
+INSERT INTO `info_publish` VALUES (4, 5, '英语', '高三', '高三英语', '星期二,星期四', '下午', '英语教学能手', '/uploads/1717848365924_OIP.jpg');
+INSERT INTO `info_publish` VALUES (5, 6, '生物', '高三', '高三生物', '星期三,星期五', '晚上', '高中生物速成', '/uploads/1717848365924_OIP.jpg');
 
 -- ----------------------------
 -- Table structure for menu
@@ -88,11 +88,11 @@ CREATE TABLE `menu`  (
 -- ----------------------------
 -- Records of menu
 -- ----------------------------
-INSERT INTO ssm.menu (id, menuCode, menuName, menuLevel, menuParentCode, menuClick, menuRight, menuComponent, menuIcon) VALUES (1, '001', '教师管理', '1', null, 'Teacher', '0', 'TeacherManage', 'el-icon-s-custom');
-INSERT INTO ssm.menu (id, menuCode, menuName, menuLevel, menuParentCode, menuClick, menuRight, menuComponent, menuIcon) VALUES (2, '002', '学生管理', '1', null, 'User', '0', 'UserManage', 'el-icon-user-solid');
-INSERT INTO ssm.menu (id, menuCode, menuName, menuLevel, menuParentCode, menuClick, menuRight, menuComponent, menuIcon) VALUES (3, '003', '家教信息', '1', null, 'InfoPublish', '0,1,2', 'InfoPublishManage', 'el-icon-takeaway-box');
-INSERT INTO ssm.menu (id, menuCode, menuName, menuLevel, menuParentCode, menuClick, menuRight, menuComponent, menuIcon) VALUES (4, '004', '学生信息', '1', null, 'DemandPublish', '0,1,2', 'DemandPublishManage', 'el-icon-takeaway-box');
-INSERT INTO ssm.menu (id, menuCode, menuName, menuLevel, menuParentCode, menuClick, menuRight, menuComponent, menuIcon) VALUES (5, '005', '评价反馈', '1', null, 'Review', '0,1,2', 'ReviewManage', 'el-icon-office-building');
+INSERT INTO `menu` VALUES (1, '001', '教师管理', '1', NULL, 'Teacher', '0', 'TeacherManage', 'el-icon-s-custom');
+INSERT INTO `menu` VALUES (2, '002', '学生管理', '1', NULL, 'User', '0', 'UserManage', 'el-icon-user-solid');
+INSERT INTO `menu` VALUES (3, '003', '家教信息', '1', NULL, 'InfoPublish', '0,1,2', 'InfoPublishManage', 'el-icon-takeaway-box');
+INSERT INTO `menu` VALUES (4, '004', '学生信息', '1', NULL, 'DemandPublish', '0,1,2', 'DemandPublishManage', 'el-icon-takeaway-box');
+INSERT INTO `menu` VALUES (5, '005', '评价反馈', '1', NULL, 'Review', '0,1,2', 'ReviewManage', 'el-icon-office-building');
 
 -- ----------------------------
 -- Table structure for review
@@ -112,7 +112,7 @@ INSERT INTO `review` VALUES (1, 'why', '系统简洁易用，赞！');
 INSERT INTO `review` VALUES (2, '张三', '太棒了！');
 
 -- ----------------------------
--- Table structure for User
+-- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
@@ -129,7 +129,7 @@ CREATE TABLE `user`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
--- Records of User
+-- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (1, 'admin', '教学管理员', '123', 18, 1, '15946284922', 0, 'Y');
 INSERT INTO `user` VALUES (2, 'zhangsan', '张三', '123456', 22, 1, '15529782988', 1, 'Y');
